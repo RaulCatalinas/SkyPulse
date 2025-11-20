85406459a1912027cca1b5fba7b4056404219399
Raul Catalinas <raulcatalinas@gmail.com>
2025-11-21 00:48:50 +0100

    feat: add logic to request approximate location permission, pending verification

--- app/src/main/java/com/example/skypulse/permissions/LocationPermission.kt
+++ app/src/main/java/com/example/skypulse/permissions/LocationPermission.kt
@@ -1,2 +1,39 @@
 package com.example.skypulse.permissions
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+
+@Composable
+fun rememberLocationPermission(): Pair<Boolean, () -> Unit> {
+    val context = LocalContext.current
+    var permissionsGranted by remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        permissionsGranted = isGranted
+    }
+
+    LaunchedEffect(Unit) {
+        permissionsGranted = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    val requestPermission = {
+        permissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
+
+    return Pair(permissionsGranted, requestPermission)
+}
